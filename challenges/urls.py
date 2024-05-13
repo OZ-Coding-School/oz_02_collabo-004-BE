43787be7dd0f8cf56c11300221ba3599e51e4799
BE_lha <155089037+LEE-Hyuna1229@users.noreply.github.com>
2024-05-14 01:10:33 +0900

    Revert "Challenge create"

--- challenges/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CreateChallenge
-
-urlpatterns = [
-    # 챌린지 생성하기 # 챌린지 정보 업데이트 하기, # 전체 챌린지 리스트 가져오기,  
-    path("challenge/create/<int:book_id>",CreateChallenge.as_view(), name='create_challenge'),
-]
