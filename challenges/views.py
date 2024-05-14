ea9ac166382e46fb8218b308824d2347c5a79e60
LEE-Hyuna1229 <yung00420@naver.com>
2024-05-14 14:29:43 +0900

    challenge merge용

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
-from rest_framework import generics, status
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.generics import get_object_or_404
 from rest_framework.exceptions import NotFound
