115f65ee9a1b73010bd501eb2654872aad9fd89c
LEE-Hyuna1229 <yung00420@naver.com>
2024-05-14 10:33:21 +0900

    #10-1

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateChallenge
+from .views import CreateChallenge, ChallengeList
 
 urlpatterns = [
-    # 챌린지 생성하기 # 챌린지 정보 업데이트 하기, # 전체 챌린지 리스트 가져오기,  
+    # 챌린지 생성하기 # 전체 챌린지 리스트 가져오기 # 챌린지 정보 업데이트 하기  
     path("challenge/create/<int:book_id>",CreateChallenge.as_view(), name='create_challenge'),
+    path("challenges/", ChallengeList.as_view(), name='challenge_list'),
 ]
