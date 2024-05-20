bb16fb6abb706c44a73fc8ec46d253b449dbf799
hotbari <138793425+hotbari@users.noreply.github.com>
2024-05-20 18:19:10 +0900

    Feat: IsStaffOrReadOnly 로직 추가(#106)

--- reviews/views.py
+++ reviews/views.py
@@ -2,26 +2,28 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import ReviewSerializer
 from .models import Review
-from books.views import custom_handle_exception
+from books.views import custom_handle_exception, IsStaffOrReadOnly
 from django.shortcuts import get_object_or_404
 from books.models import Book
 
 # 생성하기
 class CreateReview(generics.CreateAPIView):
     serializer_class = ReviewSerializer
+    permission_classes = [IsStaffOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save()
 
-    # handle_exception = custom_handle_exception
+    handle_exception = custom_handle_exception
 
 # 수정 및 불러오기
 class ReviewDetail(generics.RetrieveUpdateAPIView):
 
     serializer_class = ReviewSerializer
+    permission_classes = [IsStaffOrReadOnly]
     queryset = Review.objects.all()
 
-    # handle_exception = custom_handle_exception
+    handle_exception = custom_handle_exception
 
 
 # 책 별 추천 서평 조회
