0006690a5a33a923ee192a23f0e57e239afb7db6
hotbari <138793425+hotbari@users.noreply.github.com>
2024-05-20 18:21:49 +0900

    Feat: IsStaffOrReadOnly 로직 추가(#106)

--- spoilers/views.py
+++ spoilers/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import SpoilerSerializer
 from .models import Spoiler
-from books.views import custom_handle_exception
+from books.views import custom_handle_exception, IsStaffOrReadOnly
 
 # 생성하기
 class CreateSpoiler(generics.CreateAPIView):
     serializer_class = SpoilerSerializer
+    permission_classes = [IsStaffOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save()
@@ -18,6 +19,7 @@ class CreateSpoiler(generics.CreateAPIView):
 class SpoilerDetail(generics.RetrieveUpdateAPIView):
 
     serializer_class = SpoilerSerializer
+    permission_classes = [IsStaffOrReadOnly]
     queryset = Spoiler.objects.all()
 
     handle_exception = custom_handle_exception
